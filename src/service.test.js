2a0b460602ca2764c241847cffc7706edd6298d1
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-11-26 21:27:13 -0300

    Compare between Expected and Result

--- src/service.test.js
+++ src/service.test.js
@@ -2,6 +2,7 @@ const Service = require("./service");
 const sinon = require("sinon");
 const BASE_URL_1 = "https://swapi.dev/api/planets/1";
 const BASE_URL_2 = "https://swapi.dev/api/planets/2";
+const { deepStrictEqual } = require("assert");
 const mocks = {
   tatooine: require("./mocks/tatooine.json"),
   alderaan: require("./mocks/alderaan.json"),
@@ -16,7 +17,21 @@ const mocks = {
   stub.withArgs(BASE_URL_1).resolves(mocks.tatooine);
   stub.withArgs(BASE_URL_2).resolves(mocks.alderaan);
   {
-    const response = await service.makeRequest(BASE_URL_2);
-    console.log(response);
+    const result = await service.getPlanets(BASE_URL_1);
+    const expected = {
+      name: "Tatooine",
+      surfaceWater: "1",
+      appearedIn: 5,
+    };
+    deepStrictEqual(result, expected);
+  }
+  {
+    const result = await service.getPlanets(BASE_URL_2);
+    const expected = {
+      name: "Alderaan",
+      surfaceWater: "40",
+      appearedIn: 2,
+    };
+    deepStrictEqual(result, expected);
   }
 })();
