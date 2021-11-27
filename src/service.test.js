d5927da1217e39e8c41d9b33103a01143c8075ae
Leonardo Albuquerque <leeodesign@hotmail.com>
2021-11-26 21:11:42 -0300

    Test without Sinon

---
+++ src/service.test.js
@@ -0,0 +1,8 @@
+const Service = require("./service");
+const BASE_URL_1 = "https://swapi.dev/api/planets/1";
+const BASE_URL_2 = "https://swapi.dev/api/planets/2";
+(async () => {
+  // const service = new Service();
+  // const responseWithoutStub = await service.makeRequest(BASE_URL_2);
+  // console.log(JSON.stringify(responseWithoutStub));
+})();
